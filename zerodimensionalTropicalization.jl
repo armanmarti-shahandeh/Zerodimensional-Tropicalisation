6f5d0e416ee8cf94a407b2a2f9044adc34141a4a
Arman Marti-Shahandeh <armanpasha@hotmail.com>
2024-08-20 17:51:07 +0100

    Working Triangular set Implementations
    Both the one debugged at the meeting, as well as a new version using the struct formatting.

--- zerodimensionalTropicalization.jl
+++ zerodimensionalTropicalization.jl
@@ -1,3 +1,8 @@
+ ##There is still the question to be raised as to how (in the non-unicity case) we will append the z_i's, as these will be of different data types, so can not simply be overlaid with one another##
+##Think it might be best to create separate structures that deal with the root-finding of the algebraic varieties, such that they can then be valuated and moved into the Partially Tropicalised Point when permitted##
+##This will also nicely allow for any specific root-finding algorithms to be slotted into the implementation##
+
+
 struct PartiallyTropicalizedPoint
     partialPoint::Vector     # partially tropicalized point
     triangularSystem::Vector # triangular set of polynomials
@@ -18,23 +23,39 @@ function triangular_system(p::PartiallyTropicalizedPoint)
     return p.triangularSystem
 end
 
-function partial_tropicalization_step(p::PartiallyTropicalizedPoint)
-
+function partial_tropicalization_step(p::PartiallyTropicalizedPoint,PolyRing, nu) 
+    next_iteration_points = [] ##This will return the list of all branching partially-tropicalised points emanating from this iteration step##
     partialPoint = partial_point(p)
     triangularSystem = triangular_system(p)
-
-    # Step 1: substitute all roots that have been computed so far
-
-    # Step 2: check whether the valuations of the next variable to be tropicalized
-    #   is uniquely determined by the preceeding valuations
-
-    # Step 2a: if yes, compute all possible root valuations
-    #   and return the more complete points
-
-    # Step 2b: if no, compute all preceeding roots,
-    #   compute all possible root valuations
-    #   and return the more complete points
-
+    T = tropical_semiring()
+    variable_list = gens(PolyRing)
+    Rt, d = polynomial_ring(T, length(triangularSystem)) ##This polynomial ring has to be explicitly defined for our subsequent homomorphisms into the below univariate ring##
+    R, active = polynomial_ring(T, ["z"]) ##A univariate tropical polynomial ring to act as the vessel for all hypersurface vertex calculations##
+    active = active[1]
+    mapping_list = zeros(R, length(triangularSystem)) ##The list for the given homomorphism mapping##
+    index = length(partialPoint)+1
+    winit = Rational{Int}.(vcat([partialPoint[j] for j in 1:index-1], zeros(QQ, length(triangularSystem)-index+1)))
+    if index==1 ##The initial step of the algorithm##
+        mapping_list[1] = active
+        phi = hom(Rt, R, c->c, mapping_list)
+        for verts in vertices(tropical_hypersurface(phi(tropical_polynomial(triangularSystem[1], nu))))
+            push!(next_iteration_points, PartiallyTropicalizedPoint([verts[1]], triangularSystem))
+        end
+    elseif all([is_monomial(initial(coeff(triangularSystem[index], [variable_list[index]], [j]), nu, winit)) for j in 1:degree(triangularSystem[index], variable_list[index])])
+        for j in 1:index-1
+            mapping_list[j] = R(partialPoint[j])##This is setting up the substitution map to be used in the homomorphism##
+        end
+        mapping_list[index] = active
+        phi = hom(Rt, R, c->c, mapping_list)
+        for verts in vertices(tropical_hypersurface(phi(tropical_polynomial(triangularSystem[index], nu))))
+            new_partialPoint = copy(partialPoint)
+            push!(new_partialPoint, verts[1])
+            push!(next_iteration_points, PartiallyTropicalizedPoint(new_partialPoint, triangularSystem))
+        end
+    else
+        println("This input has yielded a non-unicity calculation, and will require setting-specific algorithms to efficiently compute the (finitely many) points in the algebraic variety")
+    end
+    return next_iteration_points
 end
 
 
@@ -43,17 +64,22 @@ function is_fully_determined(p::PartiallyTropicalizedPoint)
 end
 
 
-function zerodimensional_tropicalization(triangularSystem::Vector)
-
-    workingList = [PartiallyTropicalizedPoint(triangularSystem)]
-    while !all(is_fully_determined, workingList)
-        # take the first point in the working list
-        # apply the partial tropicalization step
-        # add the new partially tropicalized points to the working list
+function zerodimensional_tropicalization(triangularSystem::Vector, PolyRing, nu) ##The main function takes the triangular set of polynomials, the fielded ring in which they live, as well as the tropical_semiring_map that will be used for their tropicalisation##
+    determination_list = [PartiallyTropicalizedPoint(triangularSystem)]
+    while !all(is_fully_determined, determination_list)
+        copy_determination_list = []
+        for part_point in determination_list
+            for new_pts in partial_tropicalization_step(part_point, PolyRing, nu) 
+                push!(copy_determination_list, new_pts)
+            end
+        end
+        determination_list = copy(copy_determination_list)
     end
-
-    # make sure that all points are fully tropicalized
-    # i.e., replace all computed roots by their valuations
-
-    # return the tropicalizations
+    return [p.partialPoint for p in determination_list]
 end
+
+K,t = rational_function_field(QQ,"t")
+nu = tropical_semiring_map(K, t)
+R,(x1,x2,x3) = K["x1","x2","x3"]
+triangular = [t*x1^2+x1+1, t*x2^2+x1*x2+1, x3+x1*x2]
+zerodimensional_tropicalization(triangular, R, nu)
