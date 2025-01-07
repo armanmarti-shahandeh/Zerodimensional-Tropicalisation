c0d121ee56f131e03a50d9e882445208c32ad1fe
Yue Ren <yue.ren.kl@gmail.com>
2025-01-07 11:26:36 +0000

    chg: removed old files, added new test file with examples

---
+++ tst.jl
@@ -0,0 +1,29 @@
+using Oscar
+
+K,t = puiseux_series_field(QQ,100,"t")
+# nu = tropical_semiring_map(K, t)
+
+
+###
+# Example with uniquely determined tropicalization
+###
+R,(x1,x2,x3) = K["x1","x2","x3"]
+triangularSystem = [t*x1^2+x1+1, t*x2^2+x1*x2+1, x3+x1*x2]
+
+###
+# Example where increased precision in ~z1 is needed
+###
+R,(x1,x2) = K["x1","x2"]
+triangularSystem = [x1-(1+t+t^2+t^3), x2-(x1-1-t)]
+
+###
+# Example where increase precision in ~z1 is needed
+#   and old branches split
+###
+R,(x1,x2) = K["x1","x2"]
+triangularSystem = [x1^2-1, x2-(x1-1-t)]
+
+
+
+include("src/zerodimensionalTropicalization.jl")
+zero_dimensional_triangular_tropicalization(triangularSystem,QQ(9))
