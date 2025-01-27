61b7ad0d4dbf6b8e899b0478ad31cdea2b8cb189
Yue Ren <yue.ren.kl@gmail.com>
2025-01-27 11:51:33 +0000

    chg: surpressing output when going through file line by line

--- tst.jl
+++ tst.jl
@@ -33,8 +33,8 @@ println(tropical_variety_zerodimensional_triangular(triangularSystem, QQ(10)))
 
 Kt,t = puiseux_series_field(QQ, 100, "t")
 R, (x1, x2, x3, x4) = polynomial_ring(Kt, ["x1", "x2", "x3", "x4"])
-f1 = (x1-t/(1-t))^3
-f2 = (x1 + t^-1 - t/(1-t))*(t^2*x2^2+t*x2+t)
-f3 = (x1 + t^5 - t/(1-t))*(t^-4*x3^2+t^-5*x3*x2+t^-5)
-f4 = (x1+t^4-t/(1-t))*(t^-4*x4+t^-4*x3*x2)
+f1 = (x1-t/(1-t))^3;
+f2 = (x1 + t^-1 - t/(1-t))*(t^2*x2^2+t*x2+t);
+f3 = (x1 + t^5 - t/(1-t))*(t^-4*x3^2+t^-5*x3*x2+t^-5);
+f4 = (x1+t^4-t/(1-t))*(t^-4*x4+t^-4*x3*x2);
 println(tropical_variety_zerodimensional_triangular([f1, f2, f3, f4], QQ(100), QQ(1//2)))
