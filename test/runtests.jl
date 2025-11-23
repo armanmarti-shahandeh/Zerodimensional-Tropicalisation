55400c2a77bf5c6ade12b5989a44db24bb94263e
Yue Ren <yue.ren.kl@gmail.com>
2025-11-23 18:08:42 +0000

    fix: tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -20,21 +20,21 @@ using Oscar
         f1 = (x1+t^3)*(x1+1+t^4)
         f2 = x2 - x1
         I = ideal([f1,f2])
-        TropI = tropical_points_tadic_triangular(I, nu, precision=7, precisionStep=1)
+        TropI = tropical_points_triangular(I, nu, precision=7, precisionStep=1)
         @test TropI == [QQFieldElem[0,0], QQFieldElem[3,3]]
 
         # checking case with branching at higher precisions
         f1 = (x1+(1+2*t+t^2))*(x1+(1+2*t+t^3))
         f2 = x2 - (x1+1+2*t)
         I = ideal([f1,f2])
-        TropI = tropical_points_tadic_triangular(I, nu, precision=7, precisionStep=1)
+        TropI = tropical_points_triangular(I, nu, precision=7, precisionStep=1)
         @test TropI == [QQFieldElem[0,2], QQFieldElem[0,3]]
 
         # checking case with non-trivial multiplicity (currently not implemented)
         f1 = (x1+(1+2*t+t^2))*(x1+(1+2*t+t^2))
         f2 = x2 - (x1+1+2*t)
         I = ideal([f1,f2])
-        TropI = tropical_points_tadic_triangular(I, nu, precision=7, precisionStep=1)
+        TropI = tropical_points_triangular(I, nu, precision=7, precisionStep=1)
         @test TropI == [QQFieldElem[0,2]]
     end
 end
